92794ad0d7d4c0d924c0995fa86ec1aba9b7acb3
Michael Soumah <mikesoum73@gmail.com>
2024-11-23 01:08:49 -0500

    ajout du fichiers MVC et de la route index.js

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const expresslayouts = require('express-ejs-layouts')
 
+const indexRouter = require ('./routes/index')
+
 app.set('view engine','ejs')
 app.set('view',__dirname + '/views')
 app.set('layout', 'layouts/layout')
 
 app.use(expresslayouts)
 app.use(express.static('public'))
+
+app.use('/',indexRouter)
+
 app.listen(process.env.PORT || 3000)
