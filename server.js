39f3540d25903de893b5d2bcbbd80069221a57a7
Michael Soumah <mikesoum73@gmail.com>
2024-11-23 02:17:34 -0500

    fix : Erreurs orthographe views.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
-const expresslayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require ('./routes/index')
 
 app.set('view engine','ejs')
-app.set('view',__dirname + '/views')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
-app.use(expresslayouts)
+app.use(expressLayouts)
 app.use(express.static('public'))
 
 app.use('/',indexRouter)
